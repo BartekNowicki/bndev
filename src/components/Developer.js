3d97a0d353490715bb8caf1e3cb79a91d485b9bd
Bartosz Nowicki <b_nowicki@op.pl>
2020-08-16 16:59:51 +0200

    initial commit

---
+++ src/components/Developer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Developer = props => {
+  return (
+	<>
+    <div className="centralFigureWrap">centralFigureWrap</div>
+    </>
+  );
+}
+
+export default Developer;
