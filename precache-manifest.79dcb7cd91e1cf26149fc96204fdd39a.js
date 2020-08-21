ffc1dc322786e9d3a8b83a0e490341ee5eb1cbdd
Bartosz Nowicki <b_nowicki@op.pl>
2020-08-21 21:13:01 +0200

    Updates

---
+++ precache-manifest.79dcb7cd91e1cf26149fc96204fdd39a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e4aa3ddd452a10a83594a19af261e9",
+    "url": "/bndev/index.html"
+  },
+  {
+    "revision": "8b8c8ca2296b487f6db8",
+    "url": "/bndev/static/css/main.82eb0558.chunk.css"
+  },
+  {
+    "revision": "6c6aad7c2b8e74659a40",
+    "url": "/bndev/static/js/2.33de33f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bndev/static/js/2.33de33f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8c8ca2296b487f6db8",
+    "url": "/bndev/static/js/main.2109cdeb.chunk.js"
+  },
+  {
+    "revision": "b859aa736c0bda36bbc1",
+    "url": "/bndev/static/js/runtime-main.af0fa032.js"
+  },
+  {
+    "revision": "6c3a9ae3a673f830f3d807e71dcbc89f",
+    "url": "/bndev/static/media/BartoszNowicki.6c3a9ae3.png"
+  },
+  {
+    "revision": "5f03f27fc93ef3630bdde7b61ab2094c",
+    "url": "/bndev/static/media/arrow.5f03f27f.svg"
+  },
+  {
+    "revision": "d15ffdd7a16689f6407e0e335aaccbc4",
+    "url": "/bndev/static/media/astro.d15ffdd7.svg"
+  },
+  {
+    "revision": "19734bca0a5d6fa54b6c8e4e80b2be2f",
+    "url": "/bndev/static/media/bow-tie.19734bca.svg"
+  },
+  {
+    "revision": "918a9f9d8776f8469ce717d6b0528c30",
+    "url": "/bndev/static/media/closeBtn.918a9f9d.svg"
+  },
+  {
+    "revision": "df62ea59c593a164d936cb4fdf0ef98b",
+    "url": "/bndev/static/media/contacts.df62ea59.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/bndev/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/bndev/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "d96d642c581f4fe8c9bae1320c071ee6",
+    "url": "/bndev/static/media/green.d96d642c.svg"
+  },
+  {
+    "revision": "4e2310faf90ec6659798195fd296b639",
+    "url": "/bndev/static/media/house.4e2310fa.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/bndev/static/media/html5.676e8188.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/bndev/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/bndev/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/bndev/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "8caa66d1b26ba5aa763dd8b616c89355",
+    "url": "/bndev/static/media/phone.8caa66d1.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/bndev/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "c76894565291044a2b8f4a88518dcd93",
+    "url": "/bndev/static/media/scroll.c7689456.svg"
+  },
+  {
+    "revision": "f3dce133d812217b063ce21716355285",
+    "url": "/bndev/static/media/svg.f3dce133.svg"
+  },
+  {
+    "revision": "7740ac7156721c054a41320a5aa151ec",
+    "url": "/bndev/static/media/thoughtCloudLeft.7740ac71.svg"
+  },
+  {
+    "revision": "6351e276e6336ab222610d7c32c86605",
+    "url": "/bndev/static/media/thoughtCloudRight.6351e276.svg"
+  },
+  {
+    "revision": "53b662cc28a98ce0d01880e53bd17b56",
+    "url": "/bndev/static/media/write.53b662cc.svg"
+  }
+]);
